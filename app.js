5714896f774fd185cffdcb0c8f38e5eb986602df
Benjamin Cahill <benjaminacahill@gmail.com>
2017-04-24 15:56:19 -0700

    added races in js and base colors in css

--- app.js
+++ app.js
@@ -9,36 +9,36 @@ function Character(name, race, gender, charClass, align) {
   this.charClass = charClass;
   this.align = align;
   this.size;
-  this.strength;
-  this.dexterity;
-  this.constitution;
-  this.intelligence;
-  this.wisdom;
-  this.charisma;
+  this.strength = 0;
+  this.dexterity = 0;
+  this.constitution = 0;
+  this.intelligence = 0;
+  this.wisdom = 0;
+  this.charisma = 0;
 }
 
 Character.prototype.setStrength = function() {
-  this.strength = rollDice();
+  this.strength += rollDice();
 };
 
 Character.prototype.setDexterity = function() {
-  this.dexterity = rollDice();
+  this.dexterity += rollDice();
 };
 
 Character.prototype.setConstitution = function() {
-  this.constitution = rollDice();
+  this.constitution += rollDice();
 };
 
 Character.prototype.setIntelligence = function() {
-  this.intelligence = rollDice();
+  this.intelligence += rollDice();
 };
 
 Character.prototype.setWisdom = function() {
-  this.wisdom = rollDice();
+  this.wisdom += rollDice();
 };
 
 Character.prototype.setCharisma = function() {
-  this.charisma = rollDice();
+  this.charisma += rollDice();
 };
 
 function main() {
@@ -67,6 +67,9 @@ function generateCharacter() {
   var align = getAlignment();
 
   character = new Character(name, race, gender, charClass, align);
+  console.log(character.strength);
+  raceAttributes(race);
+  console.log(character.strength);
   return character;
 }
 
@@ -74,12 +77,6 @@ function getClass(){
   var a = document.getElementById('class');
   var getClass = a.options[a.selectedIndex].value;
   return getClass;
-}
-
-function getRace(){
-  var a = document.getElementById('race');
-  var getRace = a.options[a.selectedIndex].value;
-  return getRace;
 }
 
 function getGender(){
@@ -92,6 +89,49 @@ function getAlignment(){
   var a = document.getElementById('alignment');
   var getAlignment = a.options[a.selectedIndex].value;
   return getAlignment;
+}
+
+function getRace(){
+  var a = document.getElementById('race');
+  var getRace = a.options[a.selectedIndex].value;
+  return getRace;
+}
+
+function raceAttributes(race){
+  switch(race){
+  case 'gnome':
+    character.constitution += 2;
+    character.charisma += 2;
+    character.strength -= 2;
+    character.size = 'small';
+    break;
+  case 'human':
+    character.size = 'medium';
+    break;
+  case 'orc':
+    character.strength += 4;
+    character.intelligence -= 2;
+    character.wisdom -= 2;
+    character.charisma -= 2;
+    character.size = 'meduim';
+    break;
+  case 'elf':
+    character.dexterity += 2;
+    character.charisma += 2;
+    character.constitution -= 2;
+    character.size = 'medium';
+    break;
+  case 'halfling':
+    character.dexterity += 2;
+    character.charisma += 2;
+    character.strength -= 2;
+    character.size = 'small';
+    break;
+  case 'dwarf':
+    character.constitution += 2;
+    character.charisma -=2;
+    character.size = 'medium';
+  }
 }
 
 main();
