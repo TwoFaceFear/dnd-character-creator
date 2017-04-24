f2a7ce4d384e66cfbc3ef0e93b2100203cc560c8
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-24 11:38:06 -0700

    added generateCharacter() method and handleSubmitClick()

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 'use strict';
 
 function Character(name, race, gender, size, charClass, align) {
-    this.name = name;
-    this.race = race;
-    this.gender = gender;
-    this.size = size;
-    this.charClass = charClass;
-    this.align = align;
-    this.strength;
-    this.dexterity;
-    this.constitution;
-    this.intelligence;
-    this.wisdom;
-    this.charisma;
+  this.name = name;
+  this.race = race;
+  this.gender = gender;
+  this.size = size;
+  this.charClass = charClass;
+  this.align = align;
+  this.strength;
+  this.dexterity;
+  this.constitution;
+  this.intelligence;
+  this.wisdom;
+  this.charisma;
 }
 
 Character.prototype.setStrength = function() {
   this.strength = rollDice();
-}
+};
 
 Character.prototype.setDexterity = function() {
   this.dexterity = rollDice();
-}
+};
 
 Character.prototype.setConstitution = function() {
   this.constitution = rollDice();
-}
+};
 
 Character.prototype.setIntelligence = function() {
   this.intelligence = rollDice();
-}
+};
 
 Character.prototype.setWisdom = function() {
   this.wisdom = rollDice();
-}
+};
 
 Character.prototype.setCharisma = function() {
   this.charisma = rollDice();
+};
+
+function main() {
+
+  var submit = document.getElementById('submit');
+  submit.addEventListener('click', handleSubmitClick);
+}
+
+function handleSubmitClick() {
+  generateCharacter();
 }
 
 function rollDice() {
@@ -46,3 +56,18 @@ function rollDice() {
   }
   return total;
 }
+
+function generateCharacter() {
+  var name = document.getElementById('name').value;
+  var race = getRace();
+  var gender = getGender();
+  var size = getSize();
+  var charClass = getClass();
+  var align = getAlign();
+
+  var character = new Character(name, race, gender, size, charClass, align);
+  return character;
+}
+
+
+main();
