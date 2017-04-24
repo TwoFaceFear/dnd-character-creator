4a3aa84e248092fa6bae49f043fcb3c02f97efff
Kyle Aardal <aardal.kyle@gmail.com>
2017-04-24 10:31:39 -0700

    created Character object with setters for attributes

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function Character(name, race, gender, size, charClass, align) {
+    this.name = name;
+    this.race = race;
+    this.gender = gender;
+    this.size = size;
+    this.charClass = charClass;
+    this.align = align;
+    this.strength;
+    this.dexterity;
+    this.constitution;
+    this.intelligence;
+    this.wisdom;
+    this.charisma;
+}
+
+Character.prototype.setStrength = function() {
+  this.strength = rollDice();
+}
+
+Character.prototype.setDexterity = function() {
+  this.dexterity = rollDice();
+}
+
+Character.prototype.setConstitution = function() {
+  this.constitution = rollDice;
+}
+
+Character.prototype.setIntelligence = function() {
+  this.intelligence = rollDice;
+}
+
+Character.prototype.setWisdom = function() {
+  this.wisdom = rollDice;
+}
+
+Character.prototype.setCharisma = function() {
+  this.charisma = rollDice;
+}
+
+function rollDice() {
+  var total = 0;
+  for(var i = 0; i < 5; i++) {
+    total += Math.floor(Math.random() * 6 + 1);
+  }
+  return total;
+}
